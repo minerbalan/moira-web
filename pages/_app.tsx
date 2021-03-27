565d2ea1531a48857e5b8a993cf423dac7cd00fb
minerbalan <kasai_spring@icloud.com>
2021-03-27 11:27:59 +0900

    ページを開いた際ユーザー情報を取得するように

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,26 @@
 import "tailwindcss/tailwind.css";
 import "../css/styles.css";
 import { AppProps } from "next/app";
-import React from "react";
-import { Provider } from "react-redux";
+import React, { useEffect } from "react";
+import { Provider, useDispatch } from "react-redux";
 import rootStore from "../redux/rootStore";
+import { fetchCurrentUser } from "../redux/currentUser/asyncActions";
+
+const AppInit = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCurrentUser());
+  }, [dispatch]);
+
+  return null;
+};
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <Provider store={rootStore}>
       <Component {...pageProps} />
+      <AppInit />
     </Provider>
   );
 };
